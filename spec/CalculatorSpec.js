8a11a5c75b3ecaf6e380f9b02badea09a52ccfa2
Katariniss <echekina@gmail.com>
2019-05-27 11:04:25 +0500

    Adds test 'returns sum of 2 min numbers in a fully valid string'

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -12,4 +12,10 @@ describe('Calculator.calculateMinsSum', function () {
 
         expect(result).toBe(100);
     });
+
+    it('returns sum of 2 min numbers in a fully valid string', function () {
+        var result = calculator.calculateMinsSum('4,0,3,19,492,-10,1');
+
+        expect(result).toBe(-10);
+    });
 });
