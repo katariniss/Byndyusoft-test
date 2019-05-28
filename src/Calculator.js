feff73861947ff3e2d9acbf7a4765ea2d46cb5eb
Katariniss <echekina@gmail.com>
2019-05-28 19:12:35 +0500

    Adds code for calculator test 'returns number if value is a single number'

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,4 +5,16 @@ Calculator.prototype.calculateMinsSum = function (list) {
     if (!list.length) {
         return 0;
     }
+
+    let min = list[0];
+
+    for (let i = 0; i < list.length; i++) {
+        const currentNumber = list[i];
+
+        if (currentNumber < min) {
+            min = currentNumber;
+        }
+    }
+    
+    return min;
 };
