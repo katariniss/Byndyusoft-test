c528cbbe27e51bd11b6991097715ffba25f3dcdc
Katariniss <echekina@gmail.com>
2019-05-28 19:27:41 +0500

    Implements main calculator algorithm

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,15 +6,28 @@ Calculator.prototype.calculateMinsSum = function (list) {
         return 0;
     }
 
-    let min = list[0];
+    let min1 = list[0];
+    let min2 = list[1];
 
-    for (let i = 0; i < list.length; i++) {
+    if (min1 > min2) {
+       min2 = min1;
+       min1 = list[1]; 
+    }
+
+    for (let i = 2; i < list.length; i++) {
         const currentNumber = list[i];
 
-        if (currentNumber < min) {
-            min = currentNumber;
+        if (currentNumber < min2) {
+            min2 = currentNumber;
+
+            if (min2 < min1) {
+                const tmp = min1;
+                
+                min1 = min2;
+                min2 = tmp;
+            }
         }
     }
-    
-    return min;
+
+    return min1 + min2;
 };
