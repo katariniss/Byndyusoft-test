ccc120cd6ab8a8f9fff84ffcfa4d5cc882468b6c
Katariniss <echekina@gmail.com>
2019-05-28 18:07:08 +0500

    Adds code for parsing a string into a list of numbers

--- src/Parser.js
+++ src/Parser.js
@@ -1,10 +1,25 @@
 function Parser() {
 }
 
-Parser.prototype.strToArrayOfNumbers = function (value) {
-    if (!value) {
+Parser.prototype.strToArrayOfNumbers = function (string) {
+    if (!string) {
         return [];
     }
 
-    return [Number(value)];
+    var result = [];
+    var buffer = '';
+
+    for (let i = 0; i < string.length; i++) {
+        const currentSymbol = string[i];
+
+        if (currentSymbol !== ',') {
+            buffer = buffer + currentSymbol;            
+        } else {
+            result.push(Number(buffer));
+            buffer = '';
+        }
+    }
+    result.push(Number(buffer));
+
+    return result;
 };
